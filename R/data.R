cf44d91ab121affb2fef623da86088ce6e6f3ced
Yida Zhang <yzhang@bayes.med.harvard.edu>
2022-06-10 13:17:49 -0400

    add dataset

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' A fitted Bayesian model.
+#'
+#' A dataset containing the fitted Bayesian model.
+#'
+#' @format A list:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"stan.model"
